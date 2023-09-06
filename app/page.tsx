ff400ca58aee2744f8ca0b626da2844d3671ddd7
behldevansh <heydevanshbehl@gmail.com>
2023-09-06 19:06:21 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Header";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Header/>
+
+      
+      {/* Board */}
+      <h1>QuickQuill</h1>
+    </main>
+  )
+}
