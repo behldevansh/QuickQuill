72e5701882fc1e90094f9a540e91cae5226190b3
behldevansh <heydevanshbehl@gmail.com>
2023-09-07 01:50:25 +0530

    header complete

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images:{
+        // domains: ['media.licdn.com'],
+        domains: ['links.papareact.com'],
+    },
+}
 
 module.exports = nextConfig
--- readme.txt
+++ readme.txt
@@ -1,3 +1,7 @@
 npx create-next-app@latest
 
 npm run dev
+
+npm install @heroicons/react
+
+npm install react-avatar --save
